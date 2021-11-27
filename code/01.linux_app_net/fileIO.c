c65673abddc5be042558e9f2ea98f92aef98a26e
chenliang1301 <chenliang1301@qq.com>
2021-11-27 23:01:41 +0800

    fileIo

--- code/01.linux_app_net/fileIO.c
+++ code/01.linux_app_net/fileIO.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main()
 {
-	char fstr[14] = {0};
-	//int ret = sprintf(fstr, "fileio_%d_%s", 10, "chen");
-
-	int ret = snprintf(fstr, 14, "fileio_%d_%s", 10, "chen");
-	printf("ret:%d, strlen(fstr):%zu\n", ret, strlen(fstr));
-	printf("fstr = %s\n", fstr);
+	char path[14] = {0};
+	int ret = sprintf(path, "fileio_%d_%s", 10, "chen");
 	
-	FILE* pf = fopen(fstr, "w+");
+	printf("ret:%d, strlen(fstr):%zu, path:%s\n", ret, strlen(path), path);
+	
+	FILE* pf = fopen(path, "w+");
 	if(NULL == pf)
 	{
 		printf("fopen fail\n");
 	}
 	char *str = "hello world";
-	int len = strlen(str);
-	ret = fwrite(str, 1, len, pf);
+	
+	ret = fwrite(str, 1, strlen(str), pf);
 	printf("ret = %d\n", ret);
 
 	fclose(pf);
